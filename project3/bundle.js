838e7e66fae2e2495217e5794507f430327d139e
MarcoOlivette <marco.vieira.olivette@gmail.com>
2020-09-11 15:59:22 -0300

    project3:

--- project3/bundle.js
+++ project3/bundle.js
@@ -1,23 +1,22 @@
 "use strict";
 
 var arr = [1, 23, 4, 3, 2, 3, 1, 2, 21, 34, 23, 2, 21, 1, 32, 4, 12, 3214, 6, 4];
+
+function teste(i, j) {
+  return i + j;
+}
+
 var newArr = arr.map(function (item, index) {
-  return item + index;
+  return teste(item, index);
 });
-console.log(newArr);
-newArr = arr.reduce(function (total, next) {
-  return total + next;
-});
-console.log(newArr);
-newArr = arr.filter(function (item) {
-  return item % 2 == 0;
-});
-console.log(newArr);
-newArr = arr.filter(function (item) {
-  return item % 2 == 1;
-});
-console.log(newArr);
-newArr = arr.find(function (item) {
-  return item === 1;
-});
-console.log(newArr);
+console.log(newArr); // valor padrão
+
+var soma = function soma() {
+  var a = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 2;
+  var b = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 2;
+  return a + b;
+};
+
+console.log(soma());
+console.log(soma(1));
+console.log(soma(1, 2));
--- project3/main.js
+++ project3/main.js
@@ -1,31 +1,19 @@
 const arr = [1, 23, 4, 3, 2, 3, 1, 2, 21, 34, 23, 2, 21, 1, 32, 4, 12, 3214, 6, 4];
 
+function teste(i,j){
+    return i+j;
+}
+
 let newArr = arr.map(
-    (item, index) => item + index
+    (item, index) => teste(item, index)
 );
 
 console.log(newArr);
 
-newArr = arr.reduce(
-    (total, next) => total + next
-);
+// valor padrão
 
-console.log(newArr);
+const soma = (a = 2,b = 2) => a+b;
 
-newArr = arr.filter(
-    item => item % 2 == 0
-);
-
-console.log(newArr);
-
-newArr = arr.filter(
-    item => item % 2 == 1
-);
-
-console.log(newArr);
-
-newArr = arr.find(
-    item => item === 1
-);
-
-console.log(newArr);
+console.log(soma());
+console.log(soma(1));
+console.log(soma(1,2));
