8c5fa1404fab60cc52f9590e4464db355805e4a7
MarcoOlivette <marco.vieira.olivette@gmail.com>
2020-09-11 15:09:50 -0300

    project1:

--- project1/main.js
+++ project1/main.js
@@ -1,5 +1,25 @@
-class teste{
-    metodo(){
-        
+class List{
+    constructor(){
+        this.todos = new Array();
+    }
+    add(data){
+        this.data.push(data);
+        console.log(data)
     }
 }
+
+class TodoList extends List{
+    constructor(){
+        super();
+    }
+
+    addTodo(){
+        this.todos.push('Novo todo');
+        console.log(this.todos);
+    }
+}
+
+var button = document.querySelector('button#btn');
+var todoList = new TodoList();
+
+button.onclick = todoList.add('teta');
