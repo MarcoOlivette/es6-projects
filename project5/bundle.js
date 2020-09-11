024b2e9dd21dc75cdabd2fffcc1f857331889551
MarcoOlivette <marco.vieira.olivette@gmail.com>
2020-09-11 20:11:23 -0300

    project5:

---
+++ project5/bundle.js
@@ -0,0 +1,46 @@
+"use strict";
+
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
+
+function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
+
+//Rest
+var usuario = {
+  nome: 'Marco',
+  idade: 23,
+  empresa: "olivette's company"
+};
+
+var empresa = usuario.empresa,
+    resto = _objectWithoutProperties(usuario, ["empresa"]);
+
+console.log(empresa, resto); //Spread
+
+var arr1 = [1, 2, 3, 54];
+var arr2 = [5, 6, 7, 8];
+var arr3 = [].concat(arr1, arr2);
+console.log(arr3);
+
+var usuario2 = _objectSpread(_objectSpread({}, usuario), {}, {
+  nome: "João"
+});
+
+console.log(usuario2);
+console.log("meu nome \xE9 ".concat(usuario.nome));
+var nome = 'Marco';
+var idade = 21;
+
+function createObj(nome, idade) {
+  return {
+    nome: nome,
+    idade: idade
+  };
+}
+
+console.log(createObj(nome, idade));
