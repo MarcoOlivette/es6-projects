9673a0d2338a9423f32c309232c05c69597f52b9
MarcoOlivette <marco.vieira.olivette@gmail.com>
2020-09-13 18:46:06 -0300

    exercicios-modulo3:

--- exercicios-modulo3/main.js
+++ exercicios-modulo3/main.js
@@ -1,14 +1,47 @@
-import axios from 'axios'
+import axios from 'axios';
 
-class Api {
-    static async getUserInfo(username) {
+// Funão delay aciona o .then após 1s
+const delay = () => new Promise(resolve => setTimeout(resolve, 1000));
+
+const umPorSegundo = async () => {
+    delay().then(() => {
+        console.log('1s');
+        delay().then(() => {
+            console.log('2s');
+            delay().then(() => {
+                console.log('3s');
+            });
+        })
+    });
+}
+
+umPorSegundo();
+
+// exercicio 2
+
+const getUserFromGithub = async (user) => {
+    try {
+        const response = await axios.get(`https://api.github.com/users/${user}`)
+        console.log(response);
+    } catch (err) {
+        console.log(`Usuário não existe |  ${err}`);
+    }
+}
+
+getUserFromGithub('MarcoOlivette');
+
+// exercicio 3
+
+class Github {
+    static async getRepositories(repo) {
         try {
-            const response = await axios.get(`https://api.github.com/users/${username}`);
-            console.log(response)
-        } catch (err) {
-            console.log("dfasdfasdfasdfasdfasdfadfasdfa")
+            const response = await axios.get(`https://api.github.com/repos/${repo}`);
+            console.log(response.data);
+        } catch (error) {
+            console.log('Repositório não existe');
         }
     }
 }
 
-Api.getUserInfo('MarcoOlivet546a546dfas546te')
+Github.getRepositories('rocketseat/rocketseat.com.br');
+Github.getRepositories('rocketseat/dslkvmskv');
