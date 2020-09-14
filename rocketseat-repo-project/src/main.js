2d0f891376ff5e608fac023e5b61ef03035f6d06
MarcoOlivette <marco.vieira.olivette@gmail.com>
2020-09-14 17:57:10 -0300

    rocketseat-repo-project/src/main:

--- rocketseat-repo-project/src/main.js
+++ rocketseat-repo-project/src/main.js
@@ -3,15 +3,54 @@ import axios from 'axios'
 class App {
     constructor(){
         this.repositories = [];
+
         this.formEl = document.getElementById('repo-form');
+        this.listEl = document.getElementById('repo-list');
+
         this.registerHandlers();
     }
 
     registerHandlers(){
-        
+        this.formEl.onsubmit = (event) => this.addRepository(event);
     }
 
-    addRepository(){
-        
+    addRepository(event){
+        event.preventDefault();
+        this.repositories.push({
+            name,
+            description,
+            avatar_url,
+            html_url
+        });
+
+        this.render();
+    }
+
+    render(){
+        this.listEl.innerHTML = '';
+
+        this.repositories.forEach( repo => {
+                let imgEl = document.createElement('img');
+                imgEl.setAttribute('src',repo.avatar_url);
+
+                let titleEl = document.createElement('strong');
+                titleEl.appendChild(document.createTextNode(repo.name));
+
+                let descriptionEl = document.createElement('p');
+                descriptionEl.appendChild(document.createTextNode(repo.description));
+
+                let linkEl = document.createElement('a');
+                linkEl.setAttribute('target','_blank');
+                linkEl.appendChild(document.createTextNode('Acessar'));
+
+                let listItemEl = document.createElement('li');
+                listItemEl.appendChild(imgEl);
+                listItemEl.appendChild(titleEl);
+                listItemEl.appendChild(description);
+                listItemEl.appendChild(linkEl);
+
+                this.listEl.appendChild(listItemEl);
+            }
+        );
     }
 }
